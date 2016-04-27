ac38569eaf7a15f5bbd6c267dad269e33ec67ed6
Billy Saysavath <bisaysavath@gmail.com>
2016-04-26 17:15:24 -0700

    Fix server.js issues with Heroku

--- server.js
+++ server.js
@@ -2,11 +2,12 @@ var express = require("express"),
     // bodyParser = require("body-parser"),
     // mongodb = require("mongodb"),
     app = express(),
-    mongoClient;
+    mongoClient,
+    port = process.env.PORT || 3000;
 
 // Set static route to html files.
 app.use(express.static(__dirname + "/Client"));
 
 // Start listening at port 3000
-app.listen(3000);
-console.log("Server is listening at port 3000.");
+app.listen(port);
+console.log("Server is listening at port " + port);
