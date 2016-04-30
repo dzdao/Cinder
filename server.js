3ffd154a69998dc15ce0ec52f4cae7fa6b335dfb
Holly Ho <hollyho27@gmail.com>
2016-04-29 17:13:57 -0700

    Rough draft of user profile page

--- server.js
+++ server.js
@@ -3,11 +3,61 @@ var express = require("express"),
     // mongodb = require("mongodb"),
     app = express(),
     mongoClient,
-    port = process.env.PORT || 3000;
+    users = [
+		{
+			"username": "jasondoe",
+			"name": "Jason Doe",
+			"lang": ["c++", "java"],
+			"ide": ["bracket", "vi"],
+			"hobby": ["video games, movies"]
+		},
+		{
+			"username": "janekim",
+			"name": "Jane Kim",
+			"lang": ["java", "python"],
+			"ide": ["bracket", "visual studio code", "atom"],
+			"hobby": ["video games, musics"]
+		},
+		{
+			"username": "nathanwankhade",
+			"name": "Nathan Wankhade",
+			"lang": ["c++", "python"],
+			"ide": ["vi", "code", "xcode"],
+			"hobby": ["movies, musics"]
+		},
+		{
+			"username": "nathanwankhade",
+			"name": "Nathan Wankhade",
+			"lang": ["c++", "python"],
+			"ide": ["vi", "code", "xcode"],
+			"hobby": ["movies, musics"]
+		},
+		{
+			"username": "nathanwankhade",
+			"name": "Nathan Wankhade",
+			"lang": ["c++", "python"],
+			"ide": ["vi", "code", "xcode"],
+			"hobby": ["movies, musics"]
+		},
+		{
+			"username": "nathanwankhade",
+			"name": "Nathan Wankhade",
+			"lang": ["c++", "python"],
+			"ide": ["vi", "code", "xcode"],
+			"hobby": ["movies, musics"]
+		}
+	]
+    //port = process.env.PORT || 3000;
+
 
 // Set static route to html files.
 app.use(express.static(__dirname + "/Client"));
 
 // Start listening at port 3000
-app.listen(port);
-console.log("Server is listening at port " + port);
+app.listen(3000);
+//console.log("Server is listening at port " + port);
+
+app.get("/db.json", function(req, res){
+    console.log("Server working");
+    res.send(users);
+});
